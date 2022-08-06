dc13a49674145750b3d481370c27f5afc9541575
ravi.bansal <ravibansal7717@gmail.com>
2022-08-06 22:52:57 +0530

    corrected image layout and titles

--- config.py
+++ config.py
@@ -1,4 +1,4 @@
 MODEL_DIR = "art_generator_model"
 RESIZE_DIM = (256, 256)
-DISPLAY_SIZE = (550, 370)
-ALLOWED_IMAGE_EXTENSIONS = ["png", "jpeg", "jpeg"]
+DISPLAY_SIZE = (450, 450)
+ALLOWED_IMAGE_EXTENSIONS = ["png", "jpeg", "jpg"]
--- webapp.py
+++ webapp.py
@@ -5,9 +5,8 @@ from PIL import Image
 import numpy as np
 
 def layout():
-    st.title("Facial Expression Recognition System")
-    st.sidebar.title("Facial Expression Recognition System")
-    st.sidebar.header("Upload File")
+    st.title("Generate Ukiyo-e Art From Images")
+    st.sidebar.title("Generate Ukiyo-e Art From Images")
 
 def run_web_app():
     selection = layout()
@@ -15,9 +14,13 @@ def run_web_app():
     if image_file is not None:
         image = np.array(Image.open(image_file).convert("RGB"))
         image, art = generate_art_from_image(image)
-        st.subheader("Uploaded Image")
-        st.header("Generated Art")
-        st.image(art)
+
+        col1, col2 = st.columns(2)
+        col1.header("Uploaded Image")
+        col1.image(image, use_column_width = True)
+
+        col2.header("Art")
+        col2.image(art, use_column_width = True)
 
 if __name__=="__main__":
     run_web_app()
